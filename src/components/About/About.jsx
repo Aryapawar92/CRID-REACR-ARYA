2ad0b6add38688bdff8e6185b3d75dec2b1963c3
arya <aryapawar92@gmail.com>
2024-08-13 23:26:54 +0530

    card effect done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,20 @@
 import React from "react";
+import Grid from "./CardGrid.jsx";
+import Footer from "../Footer/Footer.jsx";
+import CardGrid from "./CardGrid.jsx";
 
 function About() {
   return (
-    <div className="hero min-h-screen  pt-16 flex flex-col items-center justify-start relative">
+    <div className="hero min-h-screen pt-16 flex flex-col items-center justify-start relative">
       {/* Background Pattern with Overlay */}
       <div className="absolute inset-0 -z-10 h-full w-full bg-custom-dark-blue bg-[linear-gradient(to_right,#d0d0d0_2px,transparent_2px),linear-gradient(to_bottom,#d0d0d0_2px,transparent_2px)] bg-[size:6rem_4rem] bg-blend-overlay"></div>
-
-      <div className="absolute bottom-[-10%] left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      <div className="absolute bottom-[-10%] right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      <div
-        aria-hidden="true"
-        className="absolute inset-x-8 inset-y-12 mx-auto w-36 rotate-45 scale-x-140 scale-y-150 rounded-full bg-gradient-to-b from-pink-500 to-purple-600 opacity-80 blur-3xl"
-      ></div>
 
       <div className="mt-0.5 text-center">
         <h1 className="text-white text-4xl font-bold font-redHat">
           About
           <span className="font-redHat"> CRID</span>
         </h1>
-        <p className="mt-4 text-white text-lg max-w-xl mx-auto text-justify font-space">
+        <p className="mt-4 text-white text-lg max-w-xl mx-auto text-justify font-space text-shadow">
           CRID is a comprehensive platform dedicated to fostering community and
           innovation through research and development. Our mission is to bridge
           the gap between academia and industry, facilitating groundbreaking
@@ -26,6 +22,19 @@ function About() {
           excellence and be a part of the future of innovation.
         </p>
       </div>
+
+      <div className="mt-0.5 text-center">
+        <h1 className="text-white text-4xl font-bold font-redHat py-8">
+          Meet Our Team
+        </h1>
+      </div>
+
+      <div className="py-2 my-2">
+        <CardGrid />
+      </div>
+
+      {/* Add Footer Component */}
+      <Footer />
     </div>
   );
 }
--- src/components/About/Card.jsx
+++ src/components/About/Card.jsx
@@ -1,7 +1,45 @@
+import { Instagram, Linkedin } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Card() {
-  return <div>Card</div>;
+function Card({ item }) {
+  return (
+    <div className="relative flex flex-col text-gray-700 bg-white shadow-md rounded-xl w-80 border border-white">
+      <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg rounded-xl h-64">
+        <img
+          src={item.image}
+          alt="profile-picture"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="p-4 text-center">
+        <h4 className="block mb-2 font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900">
+          {item.name}
+        </h4>
+        <p className="block font-sans text-base font-medium leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
+          {item.role}
+        </p>
+      </div>
+      <div className="flex justify-center p-4 pt-2 gap-5">
+        {item.Linkedin && (
+          <Link
+            to={item.Linkedin}
+            className="block text-xl font-normal leading-relaxed text-blue-600 hover:text-blue-800"
+          >
+            <Linkedin />
+          </Link>
+        )}
+        {item.instagram && (
+          <Link
+            to={item.instagram}
+            className="block text-xl font-normal leading-relaxed text-purple-600 hover:text-purple-800"
+          >
+            <Instagram />
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
